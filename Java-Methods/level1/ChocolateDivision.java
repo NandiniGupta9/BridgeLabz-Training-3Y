1575db7514f89a53e8fe7eeec958b0e6c5180b28
NandiniGupta9 <nandinigupta8354@gmail.com>
2025-09-02 15:28:15 +0530

    Added NumberCheck and Chocoalate

---
+++ Java-Methods/level1/ChocolateDivision.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ChocolateDivision {
+    public static int[] findRemainderAndQuotient(int chocolates, int children) {
+        return new int[]{chocolates % children, chocolates / children};
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter chocolates and children: ");
+        int chocolates = sc.nextInt();
+        int children = sc.nextInt();
+        int[] result = findRemainderAndQuotient(chocolates, children);
+        System.out.println("Each child gets " + result[1] + " chocolates, Remaining = " + result[0]);
+    }
+}
