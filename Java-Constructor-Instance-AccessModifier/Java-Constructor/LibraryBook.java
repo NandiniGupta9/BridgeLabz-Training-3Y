55fe1994af281693779e732a8364e0bae35441a6
NandiniGupta9 <nandinigupta8354@gmail.com>
2025-09-03 17:05:27 +0530

    added Library book

---
+++ Java-Constructor-Instance-AccessModifier/Java-Constructor/LibraryBook.java
@@ -0,0 +1,28 @@
+
+public class LibraryBook {
+    String title;
+    String author;
+    double price;
+    boolean isAvailable;
+
+    public LibraryBook(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.isAvailable = true; 
+    }
+
+    public void borrowBook() {
+        if (isAvailable) {
+            isAvailable = false;
+            System.out.println(title + " has been borrowed.");
+        } else {
+            System.out.println(title + " is not available.");
+        }
+    }
+
+    public void display() {
+        System.out.println("Book: " + title + " | Author: " + author + " | Price: $" + price +
+                           " | Available: " + (isAvailable ? "Yes" : "No"));
+    }
+}
