a4fcc5e532dc3daeecc433b260925f60fe7f331d
NandiniGupta9 <nandinigupta8354@gmail.com>
2025-09-03 17:03:42 +0530

    Added carRental

---
+++ Java-Constructor-Instance-AccessModifier/Java-Constructor/CarRental.java
@@ -0,0 +1,30 @@
+
+public class CarRental {
+    String customerName;
+    String carModel;
+    int rentalDays;
+    double dailyRate = 50.0; 
+
+    
+    public CarRental() {
+        this.customerName = "Unknown";
+        this.carModel = "Standard";
+        this.rentalDays = 1;
+    }
+
+    
+    public CarRental(String customerName, String carModel, int rentalDays) {
+        this.customerName = customerName;
+        this.carModel = carModel;
+        this.rentalDays = rentalDays;
+    }
+
+    public double calculateTotalCost() {
+        return rentalDays * dailyRate;
+    }
+
+    public void display() {
+        System.out.println(customerName + " rented " + carModel +
+                           " for " + rentalDays + " days. Total cost: $" + calculateTotalCost());
+    }
+}
